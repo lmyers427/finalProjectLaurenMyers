ae6f59a5882f78e46e2a94784b807a2c43de519e
Lauren Myers <lmmyers2@destop>
2022-05-02 15:43:44 -0600

    Added cors option for glitch

--- model/config/allowedOrigins.js
+++ model/config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const allowedOrigins = [
+    'https://lacy-adaptable-society.glitch.me/',
     'https://unrivaled-bublanina-a60304.netlify.app/',
     'http://127.0.0.1:5500',
     'http://localhost:3500',
